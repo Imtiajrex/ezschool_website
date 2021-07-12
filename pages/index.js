5ec6f19367099d6848b1f6e1b34cae74ed68830a
Imtiaj Shuvo <imtiajrex@gmail.com>
2021-07-12 14:27:24 +0600

    Structure Ready

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import About from "@components/About";
+import Features from "@components/Features";
+import Hero from "@components/Hero";
+import Offers from "@components/Offers";
+import SocialProof from "@components/SocialProof";
+import Footer from "@components/Footer";
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>EZschool - A School Management Application</title>
+			</Head>
+			<Hero />
+			<SocialProof />
+			<Features />
+			<Offers />
+			<About />
+			<Footer />
+		</>
+	);
+}
